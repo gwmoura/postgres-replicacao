8e82d8f57e7f697c33f69606d2c6a54e18773608
George Moura <gwmoura@gmail.com>
2017-09-26 11:38:44 -0300

    add novas chaves e troca de chaves

--- scripts/setup_master.sh
+++ scripts/setup_master.sh
@@ -1,6 +1,12 @@
 sh /vagrant/scripts/install_postgres.sh
 # copie os aquivos de consfiguração
+psql -U postgres -c "create user replicacao REPLICATION password '12345';"
+su postgres
+key=$(cat /vargrant/slave/id_rsa.pub)
+mkdir ~/.ssh
+echo $key >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+exit
 sudo cp /vagrant/master/pg_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 sudo cp /vagrant/master/postgresql.conf /etc/postgresql/9.6/main/postgresql.conf
 service postgresql restart
-psql -U postgres -c "create user replicacao REPLICATION password '12345';"
--- scripts/setup_slave.sh
+++ scripts/setup_slave.sh
@@ -1,5 +1,12 @@
 sh /vagrant/scripts/install_postgres.sh
 sudo mkdir -p /var/lib/postgresql/archive/
+chown -R postgres:postgres /var/lib/postgresql/archive/
+su postgres
+key=$(cat /vargrant/master/id_rsa.pub)
+mkdir ~/.ssh
+echo $key >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+exit
 sudo cp /vagrant/slave/pg_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 sudo cp /vagrant/slave/postgresql.conf /etc/postgresql/9.6/main/postgresql.conf
 sudo cp /vagrant/slave/recovery.conf /etc/postgresql/9.6/main/recovery.conf
