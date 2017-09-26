09cc6e94959275a4888f94d9f30a2da98412c40c
George Moura <gwmoura@gmail.com>
2017-09-26 15:09:41 -0300

    adjusts on script to provision postgres server

--- scripts/setup_master.sh
+++ scripts/setup_master.sh
@@ -1,12 +1,11 @@
 sh /vagrant/scripts/install_postgres.sh
 # copie os aquivos de consfiguração
 psql -U postgres -c "create user replicacao REPLICATION password '12345';"
-su postgres
-key=$(cat /vargrant/slave/id_rsa.pub)
-mkdir ~/.ssh
-echo $key >> ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/authorized_keys
-exit
+key=$(cat /vagrant/slave/id_rsa.pub)
+su postgres -c "mkdir ~/.ssh"
+su postgres -c "echo $key >> ~/.ssh/authorized_keys"
+su postgres -c "chmod 0600 ~/.ssh/authorized_keys"
+sudo cp /vagrant/master/id_rsa* /var/lib/postgresql/.ssh/
 sudo cp /vagrant/master/pg_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 sudo cp /vagrant/master/postgresql.conf /etc/postgresql/9.6/main/postgresql.conf
 service postgresql restart
--- scripts/setup_slave.sh
+++ scripts/setup_slave.sh
@@ -1,12 +1,11 @@
 sh /vagrant/scripts/install_postgres.sh
 sudo mkdir -p /var/lib/postgresql/archive/
 chown -R postgres:postgres /var/lib/postgresql/archive/
-su postgres
-key=$(cat /vargrant/master/id_rsa.pub)
-mkdir ~/.ssh
-echo $key >> ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/authorized_keys
-exit
+key=$(cat /vagrant/master/id_rsa.pub)
+su postgres -c "mkdir ~/.ssh"
+su postgres -c "echo $key >> ~/.ssh/authorized_keys"
+su postgres -c "chmod 0600 ~/.ssh/authorized_keys"
+sudo cp /vagrant/slave/id_rsa* /var/lib/postgresql/.ssh/
 sudo cp /vagrant/slave/pg_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 sudo cp /vagrant/slave/postgresql.conf /etc/postgresql/9.6/main/postgresql.conf
 sudo cp /vagrant/slave/recovery.conf /etc/postgresql/9.6/main/recovery.conf
